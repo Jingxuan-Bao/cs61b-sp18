9b7b80b4a845ec7d769340e8ae01316ac8cc49b3
Jingxuan-Bao <jingxuan.bao.manchester@gmail.com>
2021-04-29 19:24:21 +0100

    hw3

--- hw3/hw3/hash/TestNiceSpreadOomage.java
+++ hw3/hw3/hash/TestNiceSpreadOomage.java
@@ -33,7 +33,7 @@ public class TestNiceSpreadOomage {
      * run this and it should pass.*/
     @Test
     public void testRandomOomagesHashCodeSpread() {
-        List<Oomage> oomages = new ArrayList<>();
+        List<Oomage> oomages = new ArrayList<Oomage>();
         int N = 10000;
 
         for (int i = 0; i < N; i += 1) {
